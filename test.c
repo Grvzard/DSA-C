fad0e10174d49c8b7443a5f9b79408a8382e3136
Grvzard <grvzard@outlook.com>
2023-03-24 23:55:04 +0800

    add "test.c" demo

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "btree.h"
+#include "dict.h"
+
+
+int main(void) {
+    // btreeTest1();
+    btreeTest2();
+    // dictTest1();
+    // dictTest2();
+    // dictTest3();
+
+    return 0;
+}
